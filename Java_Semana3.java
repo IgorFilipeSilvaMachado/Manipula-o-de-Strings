371771ecbb51339a1ab9c9e644f0c377f676db1e
IgorFilipeSilvaMachado <56729271+IgorFilipeSilvaMachado@users.noreply.github.com>
2020-06-19 19:50:32 +0100

    Add files via upload

---
+++ Java_Semana3.java
@@ -0,0 +1,49 @@
+
+
+
+
+----------Declaraçoões----------
+
+Var numArrendondar = Math.round (2.8);
+
+Var numElevado = Math.Pow (3,2);
+
+Var array = [2, 6, 7, 8, 3, 4];
+
+----------Funções----------
+
+Function arrendondar(){
+	Alert(numArrendondar);
+}
+
+Function elevado(){
+	Alert(numElevado);
+}
+
+Function minArray(){
+	Var numMin= Math.Min(....array);
+	Alert(numMin);
+}
+
+Function maxArray(){
+	Var numMax= Math.Max(....array);
+	Alert(numMax);
+}
+
+----------Manipulação DE String---------- 
+
+Function Escreve(){
+	Var exemplo = "Manipulação de strings";
+	Alert(ParseInt(exemplo));
+}
+
+Function ReturnString(){
+	Var exemplo2 = "Manipulação";
+	Alert(exemplo2.Slice(0,5));
+}
+
+Function DeleteString(){
+	Var exemplo3 = "123456789";
+	Var newExemplo = exemplo1.Substring(3,6);
+	Alert(newExemplo);
+}
